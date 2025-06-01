53fb9a5fa727bacf262d2f7163609e780d269e36
Hwang Junsu <junsu0325@naver.com>
2025-06-01 14:12:17 +0900

    setting: Initial commit

---
+++ src/main/java/com/vowchapter/VowchapterApplication.java
@@ -0,0 +1,13 @@
+package com.vowchapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VowchapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VowchapterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vowchapter/VowchapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vowchapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VowchapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
